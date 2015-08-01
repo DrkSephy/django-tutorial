48931bd7bb81dfe987fe4e7e560f93064773c47d
David Leonard <sephirothcloud1025@yahoo.com>
2015-07-31 19:26:46 -0700

    Removing old project

--- demonstration/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from app import views
-
-urlpatterns = patterns('',
-	url(r'^$', views.index, name='index'),
-	url(r'^testing/$', views.test, name='test'),
-	url(r'^profile/$', views.profile, name='profile'),
-)
