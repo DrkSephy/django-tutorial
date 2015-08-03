9ecdc204b43a8fa52631014989898f2a869e7a33
David Leonard <sephirothcloud1025@yahoo.com>
2015-08-02 19:22:00 -0700

    Removing .pyc files

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,6 @@ from app import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^testing/$', views.test, name='test'),
+	url(r'^test/$', views.test, name='test'),
 	url(r'^profile/$', views.profile, name='profile'),
 ]
