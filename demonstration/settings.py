7e8cb1e60f014155b20169760eee01e82059412b
David Leonard <sephirothcloud1025@yahoo.com>
2015-08-09 21:00:52 -0700

    Adding django_nose as a project requirement

--- demonstration/settings.py
+++ demonstration/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'django_nose',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -102,3 +103,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
