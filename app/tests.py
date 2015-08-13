bbd39e30a8cacf32b8ee41285a7a10e16f147e42
David Leonard <sephirothcloud1025@yahoo.com>
2015-08-12 20:24:25 -0700

    Testing post request for profile endpoint

--- app/tests.py
+++ app/tests.py
@@ -3,7 +3,19 @@ from django.test import Client
 
 # Create your tests here.
 
-class SimpleViewTest(TestCase):
+class ViewTests(TestCase):
 	def testAppView(self):
 		resp = self.client.get('/app/')
 		self.assertEqual(resp.status_code, 200)
+
+	def testTestView(self):
+		resp = self.client.get('/app/test/')
+		self.assertEqual(resp.status_code, 200)
+
+	def testProfile(self):
+		resp = self.client.post('/app/profile/', {'user': 'DrkSephy'} )
+		self.assertEqual(resp.status_code, 200)
+
+	def testProfileContent(self):
+		resp = self.client.post('/app/profile/', {'user': 'DrkSephy'} )
+		self.assertNotEqual(resp.content, '')
